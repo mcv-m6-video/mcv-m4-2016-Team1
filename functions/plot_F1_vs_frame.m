4fbae721478bb79f1f1fdec2caa7e0b1d59c3725
alc1218 <alc1218@gmail.com>
2015-12-12 17:19:45 +0100

    Optional Task 6 finished

--- functions/plot_F1_vs_frame.m
+++ functions/plot_F1_vs_frame.m
@@ -1,9 +1,9 @@
-function [] = plot_F1_vs_frame(F1_vector_A,F1_vector_B)
+function [] = plot_F1_vs_frame(F1_vector_A,F1_vector_B, xLabel, yLabel)
 F1_vector_A(isnan(F1_vector_A)) = 0;
 F1_vector_B(isnan(F1_vector_B)) = 0;
 
 plot(1:length(F1_vector_A), F1_vector_A, 1:length(F1_vector_B), F1_vector_B);
-xlabel('frame');
-ylabel('F1');
+xlabel(xLabel);
+ylabel(yLabel);
 legend('F1 A', 'F1 B');
 end

c2b1417aa36b07ed2ce220f4275563d8145a2f5c
Txordi00 <jfrias99@gmail.com>
2015-12-12 12:42:08 +0100

    Added functions folder and performance evaluation of the whole testA and testB.

--- main.m
+++ main.m
@@ -29,18 +29,20 @@ fileSet_B=dir(folderName_gt);
 
 %Load all entire sequence
 %INPUT
-for i=1:length(fileSet_input)
-    seq_input{i}=imread(strcat('./highway/input/',fileSet_input(i).name));   
+count=1;
+for i=1201:1400
+    seq_input{count}=imread(strcat('./highway/input/',fileSet_input(i).name));   
+    count = count+1;
 end
 
 %GROUNDTRUTH
-for i=1:length(fileSet_gt)
-    seq_gt{i}=imread(strcat('./highway/groundtruth/',fileSet_gt(i).name)); 
-    
+count=1;
+for i=1201:1400
+    seq_gt{count}=imread(strcat('./highway/groundtruth/',fileSet_gt(i).name)); 
+    count = count+1;
     % normalize GT
   %  [x,y] = find(seq_gt{i}>0);
-  %  seq_gt{i}(x,y)=1;
-    
+  %  seq_gt{i}(x,y)=1;    
 end
 
 %TESTA
@@ -57,18 +59,30 @@ end
 
 %%
 
-play_video(seq_input, seq_gt, test_A, test_B, 1);
+% play_video(seq_input, seq_gt, test_A, test_B, 1);
 %play_video(test_A, test_B, 1);
 
 
 %% TASK 1
 % False Negative, True Negative, False Positive and True Positive, the Precision, the Recall and the F1 score for the 200 frames of both test A and test B sequences
 
-[TP_A, TN_A, FN_A, FP_A, Prec_A, Rec_A, F1_A] = results_evaluation(seq_gt{1201:1400},test_A);
+TP_A_counter=0; TN_A_counter=0; FN_A_counter=0; FP_A_counter=0;
+TP_B_counter=0; TN_B_counter=0; FN_B_counter=0; FP_B_counter=0;
+assert(length(test_A)==length(test_B) && length(test_B)==length(seq_gt));
+for i=1:length(test_A)
+    [TP_A, FP_A, FN_A, TN_A] = PerformanceAccumulationPixel(test_A{i},seq_gt{i});
+    TP_A_counter=TP_A_counter+TP_A; TN_A_counter=TN_A_counter+TN_A; FN_A_counter=FN_A_counter+FN_A; FP_A_counter=FP_A_counter+FP_A;
+    
+    [TP_B, FP_B, FN_B, TN_B] = PerformanceAccumulationPixel(test_B{i},seq_gt{i});
+    TP_B_counter=TP_B_counter+TP_B; TN_B_counter=TN_B_counter+TN_B; FN_B_counter=FN_B_counter+FN_B; FP_B_counter=FP_B_counter+FP_B;
+end
+TP_A_counter, TN_A_counter, FN_A_counter, FP_A_counter
+TP_B_counter, TN_B_counter, FN_B_counter, FP_B_counter
 
-[TP_B, TN_B, FN_B, FP_B, Prec_B, Rec_B, F1_B] = results_evaluation(seq_gt{1201:1400},test_B);
-
-
+[Precision_A, Accuracy_A, Specificity_A, Recall_A, F1_A] = PerformanceEvaluationPixel(TP_A, FP_A, FN_A, TN_A);
+[Precision_B, Accuracy_B, Specificity_B, Recall_B, F1_B] = PerformanceEvaluationPixel(TP_B, FP_B, FN_B, TN_B);
+[Precision_A, Accuracy_A, Specificity_A, Recall_A, F1_A]
+[Precision_B, Accuracy_B, Specificity_B, Recall_B, F1_B]
 %% TASK 2
 
 

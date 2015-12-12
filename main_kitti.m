efd95aab8238ab095f86f819d457d76dc4e48d10
Txordi00 <jfrias99@gmail.com>
2015-12-12 16:24:03 +0100

    Added and used dev_kitti functions to show errors in optical flow.

--- main_kitti.m
+++ main_kitti.m
@@ -18,11 +18,11 @@ sq_0_train{2} = imread('./data_stereo_flow/training/image_0/000045_11.png');
 sq_1_train{1} = imread('./data_stereo_flow/training/image_0/000157_10.png');
 sq_1_train{2} = imread('./data_stereo_flow/training/image_0/000157_11.png');
 
-sq_0_train_flow_noc{1} = imread('./data_stereo_flow/training/flow_noc/000045_10.png');
-sq_0_train_flow_noc{2} = imread('./data_stereo_flow/training/flow_noc/000157_10.png'); 
+sq_0_gt_flow_noc{1} = flow_read('./data_stereo_flow/training/flow_noc/000045_10.png');
+sq_0_gt_flow_noc{2} = flow_read('./data_stereo_flow/training/flow_noc/000157_10.png'); 
 
-sq_0_m4_kitti_flow_noc{1} = imread('./results_kitti/LKflow_000045_10.png');
-sq_0_m4_kitti_flow_noc{2} = imread('./results_kitti/LKflow_000157_10.png');
+sq_0_est_kitti_flow_noc{1} = flow_read('./results_kitti/LKflow_000045_10.png');
+sq_0_est_kitti_flow_noc{2} = flow_read('./results_kitti/LKflow_000157_10.png');
 
 % Plot figure 1
 
@@ -47,22 +47,34 @@ imshow(sq_1_train{t+1});
 title('train image_1 157 11')
 
 subplot(4,2,5);
-imshow(sq_0_train_flow_noc{t});
+imshow(sq_0_gt_flow_noc{t});
 title('train kitti flow')
 
 subplot(4,2,6);
-imshow(sq_0_train_flow_noc{t+1});
+imshow(sq_0_gt_flow_noc{t+1});
 title('train kitti flow')
 
 subplot(4,2,7);
-imshow(sq_0_m4_kitti_flow_noc{t});
+imshow(sq_0_est_kitti_flow_noc{t});
 title('m4 kitti flow')
 
 subplot(4,2,8);
-imshow(sq_0_m4_kitti_flow_noc{t+1});
+imshow(sq_0_est_kitti_flow_noc{t+1});
 title('m4 kitti flow')
 
 % Task 4 Compute the mean magnitude error, MMEN, for all pixels in
 % non-ocluded areas.
+tau=3;
+f_err = flow_error(sq_0_gt_flow_noc{1},sq_0_est_kitti_flow_noc{1},tau);
+F_err = flow_error_image(sq_0_gt_flow_noc{1},sq_0_est_kitti_flow_noc{1});
+figure,imshow([flow_to_color([sq_0_est_kitti_flow_noc{1};sq_0_gt_flow_noc{1}]);F_err]);
+title(sprintf('Error: %.2f %%',f_err*100));
+figure,flow_error_histogram(sq_0_gt_flow_noc{1},sq_0_est_kitti_flow_noc{1});
+pause;
+close all;
+f_err = flow_error(sq_0_gt_flow_noc{2},sq_0_est_kitti_flow_noc{2},tau);
+F_err = flow_error_image(sq_0_gt_flow_noc{2},sq_0_est_kitti_flow_noc{2});
+figure,imshow([flow_to_color([sq_0_est_kitti_flow_noc{2};sq_0_gt_flow_noc{2}]);F_err]);
+title(sprintf('Error: %.2f %%',f_err*100));
+figure,flow_error_histogram(sq_0_gt_flow_noc{2},sq_0_est_kitti_flow_noc{2});
 
-    

17d8a4c52e64f16d3a41aaf6228f9d3f022e23e6
alc1218 <alc1218@gmail.com>
2015-12-12 15:53:15 +0100

    some corrections

--- main_kitti.m
+++ main_kitti.m
@@ -8,33 +8,21 @@ global sq_0_test
 global sq_0_train
 global sq_0_test_flow_noc
 global sq_0_train_flow_noc
+global sq_0_m4_kitti_flow_noc
 
-global sq_1_test
-global sq_1_train
-global sq_1_test_flow_noc
-global sq_1_train_flow_noc
-
-% Read image_45
-
-sq_0_test{1} = imread('./data_stereo_flow/testing/image_0/000045_10.png');
-sq_0_test{2} = imread('./data_stereo_flow/testing/image_0/000045_11.png');
+% Read image_0
 
 sq_0_train{1} = imread('./data_stereo_flow/training/image_0/000045_10.png');
 sq_0_train{2} = imread('./data_stereo_flow/training/image_0/000045_11.png');
 
-sq_0_test_flow_noc{1} = imread('./results_kitti/LKflow_000045_10.png');
-sq_0_train_flow_noc{1} = imread('./data_stereo_flow/training/flow_noc/000045_10.png');
-
-% Read image_157
-
-sq_1_test{1} = imread('./data_stereo_flow/testing/image_0/000157_10.png');
-sq_1_test{2} = imread('./data_stereo_flow/testing/image_0/000157_11.png');
-
 sq_1_train{1} = imread('./data_stereo_flow/training/image_0/000157_10.png');
 sq_1_train{2} = imread('./data_stereo_flow/training/image_0/000157_11.png');
 
-sq_1_test_flow_noc{1} = imread('./results_kitti/LKflow_000157_10.png');
-sq_1_train_flow_noc{1} = imread('./data_stereo_flow/training/flow_noc/000157_10.png');
+sq_0_train_flow_noc{1} = imread('./data_stereo_flow/training/flow_noc/000045_10.png');
+sq_0_train_flow_noc{2} = imread('./data_stereo_flow/training/flow_noc/000157_10.png'); 
+
+sq_0_m4_kitti_flow_noc{1} = imread('./results_kitti/LKflow_000045_10.png');
+sq_0_m4_kitti_flow_noc{2} = imread('./results_kitti/LKflow_000157_10.png');
 
 % Plot figure 1
 
@@ -42,60 +30,37 @@ figure(1);
 
 t = 1;
 
-subplot(3,2,1);
-imshow(sq_0_test{t});
-title('test image_0 10')
-
-subplot(3,2,3);
-imshow(sq_0_test{t+1});
-title('test image_0 11')
-
-subplot(3,2,2)
+subplot(4,2,1);
 imshow(sq_0_train{t});
-title('train image_0 10')
-
-subplot(3,2,4)
-imshow(sq_0_train{t+1});
-title('train image_0 11')
-
-subplot(3,2,5);
-imshow(sq_0_test_flow_noc{t});
-title('test kitti flow')
-
-subplot(3,2,6);
-imshow(sq_0_train_flow_noc{t});
-title('train kitti flow')
-    
-
-% Plot figure 1
-
-figure(2);
-
-t = 1;
-
-subplot(3,2,1);
-imshow(sq_1_test{t});
-title('test image_1 10')
-
-subplot(3,2,3);
-imshow(sq_1_test{t+1});
-title('test image_1 11')
-
-subplot(3,2,2)
-imshow(sq_1_train{t});
 title('train image_1 10')
 
-subplot(3,2,4)
-imshow(sq_1_train{t+1});
+subplot(4,2,3);
+imshow(sq_0_train{t+1});
 title('train image_1 11')
 
-subplot(3,2,5);
-imshow(sq_1_test_flow_noc{t});
-title('test kitti flow')
+subplot(4,2,2)
+imshow(sq_1_train{t});
+title('train image_1 157 10')
 
-subplot(3,2,6);
-imshow(sq_1_train_flow_noc{t});
+subplot(4,2,4)
+imshow(sq_1_train{t+1});
+title('train image_1 157 11')
+
+subplot(4,2,5);
+imshow(sq_0_train_flow_noc{t});
 title('train kitti flow')
+
+subplot(4,2,6);
+imshow(sq_0_train_flow_noc{t+1});
+title('train kitti flow')
+
+subplot(4,2,7);
+imshow(sq_0_m4_kitti_flow_noc{t});
+title('m4 kitti flow')
+
+subplot(4,2,8);
+imshow(sq_0_m4_kitti_flow_noc{t+1});
+title('m4 kitti flow')
 
 % Task 4 Compute the mean magnitude error, MMEN, for all pixels in
 % non-ocluded areas.

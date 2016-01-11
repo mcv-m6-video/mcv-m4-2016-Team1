be7aa0574ee315ff74170aa4c9a87c01481cae84
Delia <deliafc.91@gmail.com>
2016-01-11 23:29:46 +0100

    correction paramètres

--- week3/functions/load_parameters.m
+++ week3/functions/load_parameters.m
@@ -1,7 +1,10 @@
 function params = load_parameters()
     
+% Best configuraion for P = 90 for highway and P = 390 for fall and
+% traffic.
+
     params.alpha = [0:0.5:5];
     params.showvideos1 = false;
     params.fill_conn = 4; % could be 0 (no imfill), 4 or 8
-    params.P = 1;
+    params.P = [20:20:400];
 end

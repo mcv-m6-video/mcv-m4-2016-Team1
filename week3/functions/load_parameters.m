7a2b011b9832a7e7add88c90e17e090115c42802
scanyameres <scanyameres@gmail.com>
2016-01-12 14:03:28 +0100

    ready to be improved

--- week3/functions/load_parameters.m
+++ week3/functions/load_parameters.m
@@ -1,11 +1,11 @@
 function params = load_parameters()
     
 %     params.alpha = [0:0.5:5];
-    params.alpha = [0:2:5];
+    params.alpha = [0:0.5:5];
     params.showvideos1 = false;
     params.fill_conn = 4;           % could be 0 (no imfill), 4 or 8
     
     % size of the objects to be detected
 %     params.P = [20:50:400];         % highway 90, other 390.
-    params.P = [20:100:400];         % highway 90, other 390.
+    params.P = [390];         % highway 90, other 390.
 end

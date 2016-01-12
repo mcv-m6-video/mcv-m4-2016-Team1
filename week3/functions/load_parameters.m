eb0174a068d446be3b0b6a2f3191b9f5ca837dca
scanyameres <scanyameres@gmail.com>
2016-01-12 12:37:10 +0100

    minor changes

--- week3/functions/load_parameters.m
+++ week3/functions/load_parameters.m
@@ -1,10 +1,9 @@
 function params = load_parameters()
     
-% Best configuraion for P = 90 for highway and P = 390 for fall and
-% traffic.
-
     params.alpha = [0:0.5:5];
     params.showvideos1 = false;
-    params.fill_conn = 4; % could be 0 (no imfill), 4 or 8
-    params.P = [20:20:400];
+    params.fill_conn = 4;           % could be 0 (no imfill), 4 or 8
+    
+    % size of the objects to be detected
+    params.P = [20:20:400];         % highway 90, other 390.
 end

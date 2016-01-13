b7b8d1a4e60687e80c0c3e82d47eea9a29de275e
Delia <deliafc.91@gmail.com>
2016-01-13 23:42:55 +0100

    last changes

--- week3/functions/load_parameters.m
+++ week3/functions/load_parameters.m
@@ -3,13 +3,12 @@ function params = load_parameters()
 %     params.alpha = [0:0.5:5];
 %    params.alpha = 1.8;
     params.showvideos1 = false;
-    params.fill_conn = 0;           % could be 0 (no imfill), 4 or 8
+    params.fill_conn = 4;           % could be 0 (no imfill), 4 or 8
     params.FM = 0;
 
     params.alpha = [0:0.2:7]; %best
-    params.fill_conn = 8;           % could be 0 (no imfill), 4 or 8
-    params.opening = 0;%10;  
-    params.closing = 0;%15;
+    params.opening = 10;%10;  
+    params.closing = 15;%15;
 
     % size of the objects to be detected
 %     params.P = [20:50:400];         % highway 90, other 390.

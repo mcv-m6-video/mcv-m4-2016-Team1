a2b1c6d744efb4b344ce01819d6b5ccd9c072a6f
alc1218 <alc1218@gmail.com>
2016-01-23 13:33:31 +0100

    Some corrections to Mean shift algorithm

--- week5/functions/matlab_mean_shift.m
+++ week5/functions/matlab_mean_shift.m
@@ -2,17 +2,17 @@ function matlab_mean_shift( seq )
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
-    objectFrame = seq{1};
-    objectHSV = rgb2hsv(objectFrame);
+    frame = seq{1};
+    hsv = rgb2hsv(frame);
 
     id = [1];
-    objectRegion = [185, 100, 250, 160];  
+    objectRegion = [185, 100, 250, 160];
     
     print_bb_with_id( seq{1}, id, objectRegion(1), objectRegion(2), objectRegion(3) - objectRegion(1), objectRegion(4) - objectRegion(2) )
     
     tracker = vision.HistogramBasedTracker;
-    initializeObject(tracker, objectHSV(:,:,1) , objectRegion);
-  
+    initializeObject(tracker, hsv(:,:,1) , objectRegion);
+    
     for index_frame = 2 : length(seq)
         
         frame = seq{index_frame};
@@ -26,7 +26,7 @@ function matlab_mean_shift( seq )
         x2 = [max(bbox(3) - bbox(1), 1)];
         y2 = [max(bbox(4) - bbox(2), 1)];
 
-        print_bb_with_id( seq{1}, id, x1, y1, x2, y2 )             
+        print_bb_with_id( frame, id, x1, y1, x2, y2 )             
     end
 
 end

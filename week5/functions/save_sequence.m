4fc2abd0fc3b5d25ac82d031eab53b286a9da7c1
Txordi00 <jfrias99@gmail.com>
2016-01-24 20:29:22 +0100

    Modified traffic videos.

--- week5/functions/save_sequence.m
+++ week5/functions/save_sequence.m
@@ -1,11 +1,11 @@
-function save_sequence(sequence, name)
+function save_sequence(sequence, name, frame_rate)
 disp(['Writing sequence ',name])
 v = VideoWriter(name,'Uncompressed AVI');
-v.FrameRate = 10;
+v.FrameRate = frame_rate;
 open(v);
 for i=1:length(sequence)
 %     im = flip(sequence{i},1);
-    writeVideo(v,double(sequence{i}));    
+    writeVideo(v,im2uint8(sequence{i}));    
 end
 close(v);
 
